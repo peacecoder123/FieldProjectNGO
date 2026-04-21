2feee91d388965d0ace99769a6f05335a57b45c7
Nidhis21 <nidhisalian21@gmail.com>
2026-04-21 17:18:04 +0530

    Fix missing imports in documentation tab and repository

--- lib/features/admin/presentation/tabs/documentation_tab.dart
+++ lib/features/admin/presentation/tabs/documentation_tab.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ngo_volunteer_management/app/theme/app_colors.dart';
--- lib/features/documents/repositories/firebase_document_storage_repository.dart
+++ lib/features/documents/repositories/firebase_document_storage_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:file_picker/file_picker.dart';
